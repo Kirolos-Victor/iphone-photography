6a8313c36885f60fccda9319b7452238b2474967
victor4783807 <kirolosvictor1@gmail.com>
2024-01-19 20:26:16 +0200

    initialized the service that is responsible for getting next available achievements in the achievements controller and finished the second requirement

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -3,18 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\NextAvailableAchievementService;
 use Illuminate\Http\JsonResponse;
 
 class AchievementsController extends Controller
 {
+    public function __construct(
+            protected NextAvailableAchievementService $nextAvailableAchievementService,
+    ) {
+    }
+
     public function index(User $user): JsonResponse
     {
         return response()->json([
-            'unlocked_achievements' => $user->getUnlockedAchievements,
-            'next_available_achievements' => [],
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaining_to_unlock_next_badge' => 0
+                'unlocked_achievements'          => $user->getUnlockedAchievements,
+                'next_available_achievements'    => $this->nextAvailableAchievementService->get($user),
+                'current_badge'                  => '',
+                'next_badge'                     => '',
+                'remaining_to_unlock_next_badge' => 0,
         ]);
     }
 }
