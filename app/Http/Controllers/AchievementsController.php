3bcca8c5adf8db17733fca13c82f13a86cccfe34
victor4783807 <kirolosvictor1@gmail.com>
2024-01-19 20:45:40 +0200

    Initialized the remaining to unlock next badge service in the achievements controller and finished the fifths requirements

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Services\NextAvailableAchievementService;
 use App\Services\NextLockedBadge;
+use App\Services\RemainingToUnlockNextBadge;
 use Illuminate\Http\JsonResponse;
 
 class AchievementsController extends Controller
@@ -12,6 +13,7 @@ class AchievementsController extends Controller
     public function __construct(
             protected NextAvailableAchievementService $nextAvailableAchievementService,
             protected NextLockedBadge $nextLockedBadge,
+            protected RemainingToUnlockNextBadge $remainingToUnlockNextBadge
     ) {
     }
 
@@ -22,7 +24,7 @@ class AchievementsController extends Controller
                 'next_available_achievements'    => $this->nextAvailableAchievementService->get($user),
                 'current_badge'                  => $user->getLatestUnlockedBadge(),
                 'next_badge'                     => $this->nextLockedBadge->get($user),
-                'remaining_to_unlock_next_badge' => 0,
+                'remaining_to_unlock_next_badge' => $this->remainingToUnlockNextBadge->get($user),
         ]);
     }
 }
