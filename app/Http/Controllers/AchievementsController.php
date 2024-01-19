a65da0e54d23b7ad689a973cc87dc4c9a9c74575
victor4783807 <kirolosvictor1@gmail.com>
2024-01-19 20:40:51 +0200

    Initialized the next locked badge service in the achievement controller and finished the fourth requirement by getting the next badge

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use App\Services\NextAvailableAchievementService;
+use App\Services\NextLockedBadge;
 use Illuminate\Http\JsonResponse;
 
 class AchievementsController extends Controller
 {
     public function __construct(
             protected NextAvailableAchievementService $nextAvailableAchievementService,
+            protected NextLockedBadge $nextLockedBadge,
     ) {
     }
 
@@ -19,7 +21,7 @@ class AchievementsController extends Controller
                 'unlocked_achievements'          => $user->getUnlockedAchievements,
                 'next_available_achievements'    => $this->nextAvailableAchievementService->get($user),
                 'current_badge'                  => $user->getLatestUnlockedBadge(),
-                'next_badge'                     => '',
+                'next_badge'                     => $this->nextLockedBadge->get($user),
                 'remaining_to_unlock_next_badge' => 0,
         ]);
     }
